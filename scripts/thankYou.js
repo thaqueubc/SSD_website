72bd253643cfaacb629647f85b330781b92fbf03
Tasnuva Haque <A01174842@ad.bcit.ca>
2019-11-04 14:44:30 -0800

    Contact form for desktop is created

--- scripts/thankYou.js
+++ scripts/thankYou.js
@@ -1,6 +1,8 @@
 
-
 document.getElementById("myForm").addEventListener("submit", thankYou);
 
 function thankYou() {
-  alert("Thank you for contacting us");}
+  var firstName = document.getElementById("first_name").value;
+  var lastName = document.getElementById("last_name").value;
+  alert("Thank you " + firstName + " " + lastName + " " + "for contacting us. We will get back to you soon");
+}
